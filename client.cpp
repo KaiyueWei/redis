068d49953bcdaabc9027fea947bf7a1dd7517d0a
kaiyue <kaiyue.wei@outlook.com>
2026-01-13 06:55:47 +0000

    buf type return to uint8_t*

--- client.cpp
+++ client.cpp
@@ -21,11 +21,11 @@ static void msg(const char *msg){
 
 static void die(const char *msg){
 	int err = errno;
-	fprintf(stderr, "[%d] %s]n", err, msg);	
+	fprintf(stderr, "[%d] %s\n", err, msg);	
 	abort();
 }
 
-static int32_t read_full(int fd, char *buf, size_t n){
+static int32_t read_full(int fd, uint8_t *buf, size_t n){
 	while(n > 0){
 		ssize_t rv = read(fd, buf, n);
 		if (rv <= 0){
@@ -38,7 +38,7 @@ static int32_t read_full(int fd, char *buf, size_t n){
 	return 0;
 }
 
-static int32_t write_all(int fd, const char *buf, size_t n){
+static int32_t write_all(int fd, const uint8_t *buf, size_t n){
 	while (n > 0){
 		ssize_t rv = write(fd, buf, n);
 		if (rv <= 0){
